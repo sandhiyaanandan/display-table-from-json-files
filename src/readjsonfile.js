9a196f7c5e71e43425e68ec70204be6895542b2c
sandhiyaanandan <anandansandhiyajs@gmail.com>
2020-05-13 23:47:54 +0530

    display table from json files

---
+++ src/readjsonfile.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import spanishData from './lang/spanish.json';
+import latinData from './lang/latin.json';
+import russianData from './lang/russian.json';
+import frenchData from './lang/french.json';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import './App.css';
+
+function ReadJsonFile()
+{
+	let data = [];
+	let langList = [{lang:"spanish", data:spanishData},{lang:"latin", data:latinData},{lang:"french", data:frenchData},{lang:"russian", data:russianData}];
+	const columns= [{
+      dataField: 's_no',
+      text: 'S.No',
+      style: { backgroundColor: 'blue', color: 'white' },
+      headerStyle: { backgroundColor: 'green', color: 'white', borderLeft:'1px solid green'}
+    },
+    {
+      dataField: 'name',
+      text: ' Customer Name',
+      style: { backgroundColor: 'blue', color: 'white' },
+      headerStyle: { backgroundColor: 'green', color: 'white'}
+    }, {
+      dataField: 'mailid',
+      text: 'Email-Id',
+      style: { backgroundColor: 'blue', color: 'white' },
+      headerStyle: { backgroundColor: 'green', color: 'white'}
+    },
+     {
+      dataField: 'place',
+      text: 'Place',
+      style: { backgroundColor: 'blue', color: 'white' },
+      headerStyle: { backgroundColor: 'green', color: 'white'}
+    },
+     {
+      dataField: 'query',
+      text: 'Query',
+      style: { backgroundColor: 'blue', color: 'white' },
+      headerStyle: { backgroundColor: 'green', color: 'white'}
+    }
+    ];
+    for( let i =0; i<langList.length; i++)
+    {
+    	data.push(<p className="langText">{langList[i].lang}</p>);
+	  	data.push(<div className="tableText">
+        <BootstrapTable 
+		    	keyField="s_no"
+		    	data={langList[i].data}
+		    	striped 
+			    hover 
+			    pagination={paginationFactory()}
+			    columns={ columns } 
+         dataAlign="center"/></div>);
+    }
+	return(<div>
+			{data}
+		</div>)
+	}
+
+export default ReadJsonFile;
